0a24c789f0cb8223e1194c0108b94efa830319cd
MannySotoRuiz <sotoruiz.emmanuel@gmail.com>
2023-06-01 10:39:38 -0400

    finished initial ui for settings section

--- popup.js
+++ popup.js
@@ -1,1 +1,18 @@
-console.log("hello world content script");
+const startBtn = document.getElementById("start");
+const settingsSection = document.getElementById("settingsSection");
+const expandBtn = document.getElementById("expandBtn");
+const collapseBtn = document.getElementById("collapseBtn");
+
+// event listener to open settings
+expandBtn.addEventListener("click", function () {
+  settingsSection.style.display = "flex";
+  expandBtn.classList.add("hidden");
+  collapseBtn.classList.remove("hidden");
+});
+
+// event listener to close settings
+collapseBtn.addEventListener("click", function () {
+  settingsSection.style.display = "none";
+  expandBtn.classList.remove("hidden");
+  collapseBtn.classList.add("hidden");
+});
